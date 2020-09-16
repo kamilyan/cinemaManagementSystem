aa25ae688b18a440f9446b88664890b8e4d5077a
Maor Kamilyan <MaorKamilyan@gmail.com>
2020-09-16 12:53:40 +0300

    added members api

--- subscriptions-web-service/server/config/app.js
+++ subscriptions-web-service/server/config/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
 var moviesRouter = require('../routes/movies');
+var membersRouter = require('../routes/members');
 
 require('./seed');
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/movies', moviesRouter);
+app.use('/api/members', membersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
