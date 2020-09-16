1ff0360d0e37d64189edf82f2ee5aa1b26c4940c
Maor Kamilyan <MaorKamilyan@gmail.com>
2020-09-16 12:15:07 +0300

    added movies api

--- subscriptions-web-service/server/config/app.js
+++ subscriptions-web-service/server/config/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
+var moviesRouter = require('../routes/movies');
 
 require('./seed');
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
